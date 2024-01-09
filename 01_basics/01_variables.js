09fb8bfdbdf590780f39e769310c5cc10c835f2b
abhisinghmghvp007 <154062130+abhisinghmghvp007@users.noreply.github.com>
2024-01-09 05:31:52 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+var sccountPassword = "12345"
+accountCity = "Jaipur"
+
+// accountId = 2 // not allowd
+
+accountEmail = "hc@hc.com"
+accountPassword = "212121"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
